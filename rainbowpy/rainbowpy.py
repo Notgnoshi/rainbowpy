1c91efaf100c6f3a53505a44d1033ff7c9a4642a
Austin Gill <notgnoshi@gmail.com>
2017-12-05 15:59:47 -0700

    Clarify parser name

--- rainbowpy/rainbowpy.py
+++ rainbowpy/rainbowpy.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 import argparse
-import serial
-from parser import Parser
 import readline
+from parser import Parser
+import serial
 from completions import completer
 
 readline.set_completer(completer)
@@ -10,28 +10,28 @@ readline.parse_and_bind("tab: complete")
 
 
 def main(device):
-    parser = Parser()
+    command_parser = Parser()
     ser = serial.Serial(device, 115200)
     while True:
         try:
             command = input('rainbowpy> ')
-            struct = parser.parse(command)
+            struct = command_parser.parse(command)
             if struct is not None:
                 ser.write(struct)
         except (KeyboardInterrupt, EOFError):
             print()
             break
-    ser.write(parser.parse('primary black'))
+    ser.write(command_parser.parse('primary black'))
     ser.close()
 
 
 if __name__ == '__main__':
     VERSION = 'Rainbowpy version 0.0.2 by Austin Gill'
     DESCRIPTION = 'An RGB LED strip interpreter.'
-    parser = argparse.ArgumentParser(description=DESCRIPTION)
-    parser.add_argument('--version', '-v', action='version', version=VERSION)
-    parser.add_argument('device', default='/dev/ttyUSB0', type=str,
-                        help='Serial Device. Defaults to /dev/ttyUSB0')
+    arg_parser = argparse.ArgumentParser(description=DESCRIPTION)
+    arg_parser.add_argument('--version', '-v', action='version', version=VERSION)
+    arg_parser.add_argument('device', default='/dev/ttyUSB0', type=str,
+                            help='Serial Device. Defaults to /dev/ttyUSB0')
 
-    args = parser.parse_args()
+    args = arg_parser.parse_args()
     main(args.device)
